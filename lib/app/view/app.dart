c73a988da9a1b87c23252200b8e44831935c04ab
Wojciech Kozioł <wkoziol3ib@gmail.com>
2024-01-24 14:13:29 +0100

    removed template cubit

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/counter/counter.dart';
 import 'package:todo_app/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -16,7 +15,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const Column(),
     );
   }
 }
